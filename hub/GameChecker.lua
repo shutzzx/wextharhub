edfa26ef3bbd9b064d062ccca1b84b512a66e946
shutzzx <74911950+shutzzx@users.noreply.github.com>
2021-03-15 09:33:12 -0300

    Create GameChecker.lua

---
+++ hub/GameChecker.lua
@@ -0,0 +1,6 @@
+local VerifyGame = game.PlaceId
+
+if VerifyGame == 6186867282 then
+    print("ProjectXL")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/shutzzx/wextharhub/main/script's/ProjectXL.lua"))()
+end
