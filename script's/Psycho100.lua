60562e45636df71d128f7aabb7360eeffacb733e
shutzzx <74911950+shutzzx@users.noreply.github.com>
2021-03-17 11:49:38 -0300

    Update Psycho100.lua

--- script's/Psycho100.lua
+++ script's/Psycho100.lua
@@ -52,7 +52,7 @@ local AutoFarm = Farm.Toggle({
         
                             local PRoot = game.Players.LocalPlayer.Character.HumanoidRootPart
                             local To = {}
-                            To.CFrame = gb:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0, 4.5, 0)
+                            To.CFrame = gb:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0, 4, 0)
                             local TS = TweenInfo.new(1.2, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut)
                             local Start = TweenSistem:Create(PRoot, TS, To)
         
