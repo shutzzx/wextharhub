ef017d6d5aa5ac87509962ede4b0849243377f3a
shutzzx <74911950+shutzzx@users.noreply.github.com>
2021-03-16 23:31:32 -0300

    Update GameChecker.lua

--- hub/GameChecker.lua
+++ hub/GameChecker.lua
@@ -3,4 +3,7 @@ local VerifyGame = game.PlaceId
 if VerifyGame == 6186867282 then
     print("ProjectXL")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/shutzzx/wextharhub/main/script's/ProjectXL.lua"))()
+elseif VerifyGame == 5603739866 then
+    print("Psycho100")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/shutzzx/wextharhub/main/script's/Psycho100.lua"))()
 end
